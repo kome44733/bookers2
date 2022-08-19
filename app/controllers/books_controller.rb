66b1514b686e94eeddd0c649f04fd5b4a57906af
kome44733 <110373715+kome44733@users.noreply.github.com>
2022-08-19 15:28:20 +0000

    bookers2 [finish]

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,64 @@
+class BooksController < ApplicationController
+
+  before_action :authenticate_user!
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book)
+    else
+      @books = Book.all
+      @user = current_user
+      render:index
+    end
+  end
+
+  def index
+    @book = Book.new
+    @books = Book.all
+    @user = current_user
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render:edit
+    end
+  end
+
+  def show
+    @new_book = Book.new
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+    if @book.user == current_user
+      render "edit"
+    else
+      redirect_to books_path
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    if book.user == current_user
+    book.destroy
+      redirect_to books_path
+    else
+      redirect_to books_path
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title,:body)
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+
+  devise_for :users
+  
+  get "home/about" => "homes#about"
+  get 'users/username'
+  resources :users, only: [:show, :edit, :update,:index]
+  resources :books
+  root to: "homes#top"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
